c53e814b47ef5ebc18924ea286c2c3ae95b02313
asahi <c0a241491b@edu.teu.ac.jp>
2025-12-02 15:28:38 +0900

    happy ぶらんち

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -109,35 +109,35 @@ class Beam:  # 練習1
             screen.blit(self.img, self.rct)    
 
 
-# class Bomb:
-#     """
-#     爆弾に関するクラス
-#     """
-#     def __init__(self, color: tuple[int, int, int], rad: int):
-#         """
-#         引数に基づき爆弾円Surfaceを生成する
-#         引数1 color：爆弾円の色タプル
-#         引数2 rad：爆弾円の半径
-#         """
-#         self.img = pg.Surface((2*rad, 2*rad))
-#         pg.draw.circle(self.img, color, (rad, rad), rad)
-#         self.img.set_colorkey((0, 0, 0))
-#         self.rct = self.img.get_rect()
-#         self.rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-#         self.vx, self.vy = +1, +1
+class Bomb:
+    """
+    爆弾に関するクラス
+    """
+    def __init__(self, color: tuple[int, int, int], rad: int):
+        """
+        引数に基づき爆弾円Surfaceを生成する
+        引数1 color：爆弾円の色タプル
+        引数2 rad：爆弾円の半径
+        """
+        self.img = pg.Surface((2*rad, 2*rad))
+        pg.draw.circle(self.img, color, (rad, rad), rad)
+        self.img.set_colorkey((0, 0, 0))
+        self.rct = self.img.get_rect()
+        self.rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+        self.vx, self.vy = +1, +1
 
-#     def update(self, screen: pg.Surface):
-#         """
-#         爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
-#         引数 screen：画面Surface
-#         """
-#         yoko, tate = check_bound(self.rct)
-#         if not yoko:
-#             self.vx *= -1
-#         if not tate:
-#             self.vy *= -1
-#         self.rct.move_ip(self.vx, self.vy)
-#         screen.blit(self.img, self.rct)
+    def update(self, screen: pg.Surface):
+        """
+        爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        yoko, tate = check_bound(self.rct)
+        if not yoko:
+            self.vx *= -1
+        if not tate:
+            self.vy *= -1
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
 
 
 def main():
@@ -162,13 +162,13 @@ def main():
         for b,bomb in enumerate(bombs):
             if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(8, screen)
-                fonto = pg.font.Font(None,80)
-                txt = fonto.render("Game Over", True, (255,0,0))
-                screen.blit(txt,[WIDTH//2-150, HEIGHT//2])
-                pg.display.update()
-                time.sleep(1)
-                return
+                # bird.change_img(8, screen)
+                # fonto = pg.font.Font(None,80)
+                # txt = fonto.render("Game Over", True, (255,0,0))
+                # screen.blit(txt,[WIDTH//2-150, HEIGHT//2])
+                # pg.display.update()
+                # time.sleep(1)
+                # return
             
         for b, bomb in enumerate(bombs):
             if beam is not None:
